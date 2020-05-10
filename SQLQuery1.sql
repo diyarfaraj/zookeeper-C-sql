ca281b8563442cdd886e189b137670c0befe367f
Diyar Faraj <diyar.faraj@gmail.com>
2020-05-10 20:24:10 +0200

    another one

---
+++ SQLQuery1.sql
@@ -0,0 +1,2 @@
+﻿--select * from ZooAnimal where ZooId = 1 
+select a.Name from Animal a inner join ZooAnimal za on a.Id = za.AnimalId where za.ZooId = 1;
---
+++ WPF ZOO MANAGER 2/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_ZOO_MANAGER_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SqlConnection sqlConnection;
+        public MainWindow() 
+        {
+            InitializeComponent();
+
+            string connectionString = ConfigurationManager.ConnectionStrings["WPF_ZOO_MANAGER_2.Properties.Settings.diyDBConnectionString"].ConnectionString;
+            sqlConnection = new SqlConnection(connectionString);
+
+            ShowZoos();
+        }
+
+        private void ShowZoos()
+        {
+            try
+            {
+                string query = "select * from Zoo";
+
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, sqlConnection);
+
+                using (sqlDataAdapter)
+                {
+                    DataTable zooTable = new DataTable();
+
+                    sqlDataAdapter.Fill(zooTable);
+
+                    zooList.DisplayMemberPath = "Location";
+                    zooList.SelectedValuePath = "Id";
+                    zooList.ItemsSource = zooTable.DefaultView;
+
+                }
+            }
+            catch (Exception e)
+            {
+
+                MessageBox.Show(e.ToString());
+            }
+            
+        }
+    }
+}
